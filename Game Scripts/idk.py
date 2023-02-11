5b0fdca58aaa114b7360b0d32ef7ca48380e2c8b
OPGamerYeet <69691779+OPG-AOI@users.noreply.github.com>
2023-02-11 12:53:39 -0500

    Deleted idk.py
    It was useless

--- Game Scripts/idk.py
+++
@@ -1,3 +0,0 @@
-#this is only for my roblox games.
-#They are all open source, so you can take them and use them however you would like.
-#I woud prefer if you would add me in the credits in whatever you are going to use this for. Thanks!
