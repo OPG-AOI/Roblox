46b0b4cfc7f913ab65d51e2c3512ed724a8afcb4
OPG-AOI <69691779+OPG-AOI@users.noreply.github.com>
2023-02-15 16:17:16 -0500

    Update!!!!

--- Game Scripts/TeamChooseGUI.lua
+++ Game Scripts/TeamChooseGUI.lua
@@ -1,38 +1,37 @@
--- The name of the team to join
-local teamName = "Bamboo's Army"
-local teamName2 = "Furrballs Army"
+-- The team names and buttons
+local teams = {
+    {name = "Bamboo's Army", button = nil},
+    {name = "Furrballs Army", button = nil},
+}
 
 -- Get the player's character and the player's GUI
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local gui = player:WaitForChild("PlayerGui")
 
--- Create the UI button
-local button = Instance.new("TextButton")
-button.Text = "Join " .. teamName
-button.Size = UDim2.new(0, 200, 0, 50)
-button.Position = UDim2.new(0.5, -100, 0.5, -25)
-button.Parent = gui
+-- Create the UI buttons
+for _, team in ipairs(teams) do
+    local button = Instance.new("TextButton")
+    button.Text = "Join " .. team.name
+    button.Size = UDim2.new(0, 200, 0, 50)
+    button.Position = UDim2.new(0.5, -100, 0.5 + 60 * (_ - 1 - #teams / 2), -25)
+    button.Parent = gui
+    team.button = button
 
-local button2 = Instane.new(TextButton)
-button.Test = "Join " .. teamName2
-button/Size = UDim2.new(0, 200, 0, 50)
---Location Script here
-button.Parent = gui
+    -- When the button is clicked
+    button.MouseButton1Click:Connect(function()
+        -- Get the team to join
+        local teamObject = game:GetService("Teams"):FindFirstChild(team.name)
 
--- When the button is clicked
-button.MouseButton1Click:Connect(function()
-    -- Get the team to join
-    local team = game.Teams:FindFirstChild(teamName)
+        if teamObject then
+            -- Join the team
+            teamObject:AddPlayer(player)
+            print("You have joined the " .. team.name .. " team!")
 
-    if team then
-        -- Join the team
-        team:AddPlayer(player)
-        print("You have joined the " .. teamName .. " team!")
-
-        -- Remove the button
-        button.Parent:Destroy()
-    else
-        print("Sorry, the " .. teamName .. " team could not be found.")
-    end
-end)
+            -- Remove the button
+            button:Destroy()
+        else
+            warn("Sorry, the " .. team.name .. " team could not be found.")
+        end
+    end)
+end
